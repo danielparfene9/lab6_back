3e9fd354b73a43bd8c9c8990ab7b9426667335cf
danielparfene9 <daniel.parfene@isa.utm.md>
2025-05-19 22:20:20 +0300

    swap to localfile db

--- db/alchemy_settings.py
+++ db/alchemy_settings.py
@@ -1,5 +1,5 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, Session
 from models import Base
 import os
 
@@ -15,9 +15,17 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 def init_db():
     Base.metadata.create_all(bind=engine)
 
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
+class DBSessionSingleton:
+    _session = None
+
+    @classmethod
+    def get_session(cls) -> Session:
+        if cls._session is None:
+            cls._session = SessionLocal()
+        return cls._session
+
+    @classmethod
+    def close_session(cls):
+        if cls._session:
+            cls._session.close()
+            cls._session = None
